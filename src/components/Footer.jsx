400749f9b63a788cd526c271c9dca6b4ceb394d0
mknyambura <kariuki.mercie@gmail.com>
2023-06-03 19:38:33 +0300

    update files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,34 +15,34 @@ function Footer() {
             <div className='w-[60%] flex flex-row justify-between'>
                 <div className='text-slate-400 flex flex-col gap-2'>
                     <h1 className='text-white font-bold text-xl'>Company</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>About us</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Career</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Blog</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>FAQs</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>About us</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Career</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Blog</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>FAQs</h1>
                 </div>
 
                 <div className='text-slate-400 flex flex-col gap-2'>
                     <h1 className='text-white font-bold text-xl'>Need Help?</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Contact us</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Shipping Services</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Payment Options</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Return & Exchanges</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Unsubscribe</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Contact us</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Shipping Services</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Payment Options</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Return & Exchanges</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Unsubscribe</h1>
                 </div>
 
                 <div className='text-slate-400 flex flex-col gap-2'>
                     <h1 className='text-white font-bold text-xl'>Follow us</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Instagram</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Facebook</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Twitter</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Snapchat</h1>
-                    <h1 className='text-[18px] hover:underline hover:cursor-pointer'>Pinterest</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Instagram</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Facebook</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Twitter</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Snapchat</h1>
+                    <h1 className='text-[14px] hover:underline hover:cursor-pointer'>Pinterest</h1>
                 </div>
             </div>
         </div>
         <div className='flex flex-col items-center  justify-between gap-8 mt-24 text-slate-400'>
             <div className='bg-slate-500 h-[1px] w-full'></div>
-            <h1 className='text-[18px]'>Copyright @ Mercy Faith Nyambura Kariuki 2023. All Rights Reserved.</h1>
+            <h1 className='text-[14px]'>Copyright @ Mercy Faith Nyambura Kariuki 2023. All Rights Reserved.</h1>
         </div>
     </div>
   )
